7404816c7c9f2a68f4f2f43f24f83f5460d1edcf
Cole Whitehurst <colewhitehurst96@gmail.com>
2025-04-08 13:25:43 -0400

    professors component

--- fsa-university/src/components/Professors.jsx
+++ fsa-university/src/components/Professors.jsx
@@ -1,8 +1,67 @@
-
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getProfessors, getSingleProfessor } from "../API/professors";
 
 const Professors = () => {
+    const [professors, setProfessors] = useState([]);
+    const [searchParam, setSearchParam] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function renderProfessors() {
+            const professors = await getProfessors();
+            setProfessors(professors);
+        }
+        renderProfessors();
+    }, []);
+
+    const professorsToDisplay = searchParam ? professors.filter((professor) => professors.name.toLowerCase().includes(searchParam)) : professors;
+
+    async function handleDetails(professorId) {
+        const APIResponse = await getSingleProfessor(professorId);
+        navigate(`/professors/${professorId}`);
+    }
+
     return ( 
-        <h3>Faculty</h3>
+        <>
+      <div className="search">
+        <label>
+          Search:{" "}
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setSearchParam(e.target.value.toLowerCase())}
+          />
+        </label>
+      </div>
+      <div className="allDepts">
+        {professorsToDisplay.map((professor) => {
+          return (
+            <div key={professor.id} className="professor">
+              <h4>{professor.name}</h4>
+              <h5>{professor.description}</h5>
+              <h5>{professor.professors}</h5>
+              <h5>{professor.email}</h5>
+              <img
+                src={professor.image}
+                alt={professor.name}
+                className="dpmtPics"
+              />
+              <br />
+              <button
+                className="details"
+                onClick={() => handleDetails(professor.id)}
+              >
+                More Details
+              </button>
+              <br />
+              <br />
+            </div>
+          );
+        })}
+        ;
+      </div>
+    </>
      );
 }
  
